7afd8d2b11c131932a7ec80278d694478825dc33
Bek <kruzus@live.com>
2021-03-12 04:36:07 -0500

    first commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import { Container, Wrapper } from "./style"
+import Navbar from "../Navigation/"
+
+const Layout: React.FC = () => {
+	return (
+		<Container>
+			<Wrapper>
+				<Navbar />
+				HEY
+			</Wrapper>
+		</Container>
+	)
+}
+
+export default Layout
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+	return (
+		<>
+			<h1>This is Navigation</h1>
+		</>
+	)
+}
+
+export default Navbar
